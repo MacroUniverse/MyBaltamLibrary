d0c0789b8d2e8f2e4c2f4630fd202badbf78af1f
MacroUniverse <270174408@qq.com>
2023-10-17 16:34:19 -0500

    cat test success

--- cat.m
+++ cat.m
@@ -1,23 +1,26 @@
 function A = cat(dim, varargin)
 N = numel(varargin); % # of matrices
 if N == 0
-    error('使用格�?： A = cat(维度, A1, A2, ...)');
+    error('使用格式： A = cat(维度, A1, A2, ...)');
 end
 sz = size(varargin{1}); % size of A
-Ndim = max(dim, ndims(varargin{1})); % max dim ever needed
+Ndim = numel(sz); % max dim ever needed
+if dim > Ndim
+    sz = [sz ones(1, dim-Ndim)];
+    Ndim = dim;
+end
 sz_dim = zeros(1, N); % size(varargin{i}, dim)
-sz_dim(1) = size(varargin{1}, dim);
+sz_dim(1) = sz(dim);
 for i = 2:N
     nd = ndims(varargin{i});
     if nd > Ndim
         sz = [sz ones(1,nd-Ndim)];
         Ndim = nd;
     end
+    sz_dim(i) = size(varargin{i}, dim);
     for j = 1:Ndim
-        if j == dim
-            sz_dim(i) = size(varargin{i}, dim);
-        elseif size(varargin{i}, j) ~= sz(j)
-            error(['第 ' num2str(i) ' 个矩阵的第 ' num2str(j) ' 维长度�?符!']);
+        if j ~= dim && size(varargin{i}, j) ~= sz(j)
+            error(['第 ' num2str(i) ' 个矩阵的第 ' num2str(j) ' 维长度不符!']);
         end
     end
 end
@@ -29,12 +32,12 @@ A = zeros(sz);
 cmd = "A(";
 for i = 1:Ndim
     if i > 1
-        cmd = cmd + ',';
+        cmd = cmd + ",";
     end
     if i == dim
         cmd = cmd + "ind:ind+n-1";
     else
-        cmd = cmd + ':';
+        cmd = cmd + ":";
     end
 end
 cmd = cmd + ") = varargin{i};";
--- cat_test.m
+++ cat_test.m
@@ -1,4 +1,30 @@
-function A = cat_demo
+function cat_test
 dim = 2;
-A = cat(4, zeros(2,3,2), ones(2,3,2));
+A1 = rand(2,3,2); A2 = rand(2,3,2);
+
+A = cat(1, A1, A2);
+if ~isequal(A(1:2,:,:), A1) || ~isequal(A(3:4,:,:), A2)
+    error('failed!');
+end
+    
+A = cat(2, A1, A2);
+if ~isequal(A(:,1:3,:), A1) || ~isequal(A(:,4:6,:), A2)
+    error('failed!');
+end
+
+A = cat(3, A1, A2);
+if ~isequal(A(:,:,1:2), A1) || ~isequal(A(:,:,3:4), A2)
+    error('failed!');
+end
+
+A = cat(4, A1, A2);
+if ~isequal(A(:,:,:,1), A1) || ~isequal(A(:,:,:,2), A2)
+    error('failed!');
+end
+
+A = cat(5, A1, A2);
+if ~isequal(A(:,:,:,:,1), A1) || ~isequal(A(:,:,:,:,2), A2)
+    error('failed!');
+end
+
 end
